1fdb7baee9166fe843a1cb707a7f842aa9c2e2f8
Milan Thapa <tmilan0604@gmail.com>
2023-05-12 16:49:21 +0545

    csv file uploaded

--- src/components/charts/NormalDistChart.jsx
+++ src/components/charts/NormalDistChart.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Line } from "react-chartjs-2";
+import Papa from "papaparse";
 
 const NormalDistChart = () => {
-  return (
-    <div>NormalDistChart</div>
-  )
-}
+  const [chartData, setChartData] = useState({ labels: [], datasets: [] });
 
-export default NormalDistChart
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("https://github.com/milanthapa1996/mrime/blob/master/src/assets/data/csv_file.csv");
+      const csvData = await response.text();
+
+      Papa.parse(csvData, {
+        complete: (result) => {
+          const { data } = result;
+          const labels = data.slice(1).map((entry) => entry[0]);
+          const dataset = {
+            label: "Whole Brain Volume",
+            data: data.slice(1).map((entry) => entry[1]),
+            fill: false,
+            borderColor: "rgba(54, 162, 235, 1)",
+          };
+
+          setChartData({ labels, datasets: [dataset] });
+        },
+        header: true,
+      });
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h2 className="text-slate-800 font-medium text-lg mb-4">
+        Normal Distribution - Whole Brain Volume
+      </h2>
+      <Line data={chartData} />
+    </div>
+  );
+};
+
+export default NormalDistChart;
